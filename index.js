6eeeb1feef1c2a22dc379293a3c0dc4e79259ae9
Tri <triphan921@gmail.com>
2021-02-05 10:16:31 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+var element = document.getElementsByTagName('main');
+element[0].remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id= 'victory';
+
+var headerText = document.createTextNode('Kai is the champion');
+
+newHeader.appendChild(headerText);
